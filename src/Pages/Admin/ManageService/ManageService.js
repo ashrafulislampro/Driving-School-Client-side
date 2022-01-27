53611cec1ff0056e8d250ab5646867a1073888ad
ashrafulislampro <ashrafulislam1659@gmail.com>
2022-01-27 21:57:58 +0600

    all code is update and done

--- src/Pages/Admin/ManageService/ManageService.js
+++ src/Pages/Admin/ManageService/ManageService.js
@@ -4,11 +4,13 @@ import Sidebar2 from '../../Sidebar/Sidebar2';
 
 const ManageService = () => {
           const [loggedInUser, setLoggedInUser] = useContext(UserContext);
-          return ( <section className="container-fluid row">
-          <div className="col-md-2">
+          return ( 
+          <section className="container-fluid">
+          <div className="row">
+          <div className="col-sm-12 col-md-3 col-lg-2">
             <Sidebar2></Sidebar2>
           </div>
-          <div className="col-md-8">
+          <div className="col-sm-12 col-md-9 col-lg-8">
             <div className="d-flex justify-content-between pt-4 pb-2 ms-5">
               <h4 style={{ color: "#3A4256", fontWeight: "700" }}>Manage Service</h4>
               <h4 style={{ color: "#3A4256", fontWeight: "700" }}>
@@ -18,6 +20,7 @@ const ManageService = () => {
             <div className="common_area">
                     <h1 className="py-5 ms-5">Developer Sleeping Now...</h1>
             </div>
+          </div>
           </div>
         </section>
           );
