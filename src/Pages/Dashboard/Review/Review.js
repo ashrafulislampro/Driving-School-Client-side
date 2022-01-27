f948353704ad657d26aadd2178660f017f6048e5
ashrafulislampro <ashrafulislam1659@gmail.com>
2022-01-27 22:02:19 +0600

    all code is update and done

--- src/Pages/Dashboard/Review/Review.js
+++ src/Pages/Dashboard/Review/Review.js
@@ -63,7 +63,7 @@ const Review = () => {
                 <input
                   type="text"
                   placeholder="Your Name"
-                  className="form-control book_input"
+                  className="form-control book-input"
                   required
                   {...register("name", { required: true })}
                 />
@@ -71,7 +71,7 @@ const Review = () => {
                 <input
                   type="text"
                   placeholder="Company's name or Designation"
-                  className="form-control book_input mt-3"
+                  className="form-control book-input mt-3"
                   required
                   {...register("title", { required: true })}
                 />
